6601795bd36e93d57381687472fca30258b81c98
stvnus <stvnus98@gmail.com>
2023-10-12 17:28:16 +0700

    feat : add component category, modal dan add utils

--- app/page.js
+++ app/page.js
@@ -1,5 +1,115 @@
-import Nav from '@/app/components/molecules/nav'
+"use client";
+
+import { useState } from "react";
+
+import { currencyFormatter } from "@/app/library/utils";
+
+import ExpenseCategoryItem from "@/app/components/organism/categoryExpense";
+import Modal from "@/app/components/organism/modal";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: "Entertainment",
+    color: "#000",
+    total: 500,
+  },
+  {
+    id: 2,
+    title: "Gass",
+    color: "#009",
+    total: 200,
+  },
+  {
+    id: 3,
+    title: "Fuel",
+    color: "#000",
+    total: 1200,
+  },
+  {
+    id: 4,
+    title: "Movies",
+    color: "#000",
+    total: 800,
+  },
+  {
+    id: 5,
+    title: "Holiday",
+    color: "#000",
+    total: 2000,
+  },
+];
+
 export default function Home() {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
   return (
-    <Nav />
-  )}
+    <>
+      {/* Modal */}
+      <Modal show={modalIsOpen} onClose={setModalIsOpen}>
+        <h3>Hello World</h3>
+      </Modal>
+
+      <main className="container max-w-2xl px-6 mx-auto">
+        <section className="py-3">
+          <small className="text-gray-400 text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+        </section>
+
+        <section className="flex items-center gap-2 py-3">
+          <button
+            onClick={() => {
+              setModalIsOpen(true);
+            }}
+            className="btn btn-primary"
+          >
+            + Expenses
+          </button>
+          <button className="btn btn-primary-outline">+ Income</button>
+        </section>
+
+        {/* Expenses */}
+        <section className="py-6">
+          <h3 className="text-2xl">My Expenses</h3>
+          <div className="flex flex-col gap-4 mt-6">
+            {DUMMY_DATA.map((expense) => {
+              return (
+                <ExpenseCategoryItem
+                  color={expense.color}
+                  title={expense.title}
+                  total={expense.total}
+                />
+              );
+            })}
+          </div>
+        </section>
+
+        {/* Chart Section */}
+        <section className="py-6">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: DUMMY_DATA.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: DUMMY_DATA.map((expense) => expense.total),
+                    backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+                    borderColor: ["#18181b"],
+                    borderWidth: 5,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
